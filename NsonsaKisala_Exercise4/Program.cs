db0ec9e24092801d380c60abe0ce21d36a38e147
Nsonsa <elombekisala@gmail.com>
2019-01-20 19:41:20 -0500

    Created a randomizer method that will generate a random number based on the length of the selected color dictionary and then prompt the value associated with the random key.

--- NsonsaKisala_Exercise4/Program.cs
+++ NsonsaKisala_Exercise4/Program.cs
@@ -28,7 +28,47 @@ namespace NsonsaKisala_Exercise4
                 "7 - Violet \n" +
                 "8 - Exit \n" +
                 "----------";
-            
+            // Red Dictionary
+            Dictionary<int, string> redDict = new Dictionary<int, string>();
+            redDict.Add(0, "Fact 1");
+            redDict.Add(1, "Fact 2");
+            redDict.Add(2, "Fact 3");
+
+            // Orange Dictionary
+            Dictionary<int, string> orangeDict = new Dictionary<int, string>();
+            orangeDict.Add(0, "Fact 1");
+            orangeDict.Add(1, "Fact 2");
+            orangeDict.Add(2, "Fact 3");
+
+            // Yellow Dictionary
+            Dictionary<int, string> yellowDict = new Dictionary<int, string>();
+            yellowDict.Add(0, "Fact 1");
+            yellowDict.Add(1, "Fact 2");
+            yellowDict.Add(2, "Fact 3");
+
+            // Green Dictionary
+            Dictionary<int, string> greenDict = new Dictionary<int, string>();
+            greenDict.Add(0, "Fact 1");
+            greenDict.Add(1, "Fact 2");
+            greenDict.Add(2, "Fact 3");
+
+            // Blue Dictionary
+            Dictionary<int, string> blueDict = new Dictionary<int, string>();
+            blueDict.Add(0, "Fact 1");
+            blueDict.Add(1, "Fact 2");
+            blueDict.Add(2, "Fact 3");
+
+            // Indigo Dictionary
+            Dictionary<int, string> indigoDict = new Dictionary<int, string>();
+            indigoDict.Add(0, "Fact 1");
+            indigoDict.Add(1, "Fact 2");
+            indigoDict.Add(2, "Fact 3");
+
+            // Violet Dictionary
+            Dictionary<int, string> violetDict = new Dictionary<int, string>();
+            violetDict.Add(0, "Fact 1");
+            violetDict.Add(1, "Fact 2");
+            violetDict.Add(2, "Fact 3");
 
             // Form while loop
             bool isRunning = true;
@@ -55,6 +95,7 @@ namespace NsonsaKisala_Exercise4
                 {
                     case 1:
                         Console.WriteLine("Red");
+                        FactRandomizer(redDict);
                         break;
                     case 2:
                         Console.WriteLine("Orange");
@@ -100,6 +141,24 @@ namespace NsonsaKisala_Exercise4
                 }
                 
             }
+        }
+
+        //Method to randomize the color facts
+        public static void FactRandomizer(Dictionary<int, string> dict)
+        {
+            // Get the keys of the dictionary and put it into a list
+            List<int> keyList = new List<int>(dict.Keys);
+
+            Random rand = new Random();
+
+            // Get length of new key list
+            int length = keyList.Count;
+
+            //create a random number based off the length of the key list
+            int randomKey = keyList[rand.Next(length)];
+
+            // print back to console
+            Console.WriteLine(dict[randomKey]); 
         }
     }
 }
